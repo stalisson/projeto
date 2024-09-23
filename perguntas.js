1a7ff6454dc89b5d77d11a37f39b9ff988221d1e
Istalisson <149580590+stalisson@users.noreply.github.com>
2024-09-23 15:10:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'matematica',
+    '1+1?',
+    '2'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da alemanha?',
+    'A capital da alemanha é Berlin'
+)
+
+criaCartao(
+    'cor',
+    'Quais são as cores neutras?',
+    'Preto, Branco, Cinza'
+)
+
+criaCartao(
+    'Ciencia',
+    'Quantos planetas tem no sistema solar?',
+    'atualmente são oito planetas'
+)
