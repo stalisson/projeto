1efce1a9ce62cc30146a6648713052f50b10d87b
Istalisson <149580590+stalisson@users.noreply.github.com>
2024-09-23 15:32:37 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'matematica',
-    '1+1?',
-    '2'
+    'ciencia',
+    ' Qual é o processo pelo qual as plantas convertem luz solar em energia?',
+    'O processo pelo qual as plantas convertem luz solar em energia é a fotossíntese.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da alemanha?',
-    'A capital da alemanha é Berlin'
+    'Qual é o maior país do mundo em extensão territorial?',
+    ' O maior país do mundo em extensão territorial é a Rússia.'
 )
 
 criaCartao(
-    'cor',
-    'Quais são as cores neutras?',
-    'Preto, Branco, Cinza'
+    'Futebol',
+    'Quem é o jogador com mais gols na história da Copa do Mundo?',
+    ' O jogador com mais gols na história da Copa do Mundo é Miroslav Klose, da Alemanha, com 16 gols.'
 )
 
 criaCartao(
-    'Ciencia',
-    'Quantos planetas tem no sistema solar?',
-    'atualmente são oito planetas'
+    'Futebol',
+    'Qual clube europeu tem mais títulos da Liga dos Campeões da UEFA?',
+    'O clube europeu com mais títulos da Liga dos Campeões da UEFA é o Real Madrid, com 14 títulos.'
 )
